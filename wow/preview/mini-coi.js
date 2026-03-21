8f80b3731749b65aee4bae7c889a050bd6c7544e
FidelisAnalog <FidelisAnalog@users.noreply.github.com>
2026-03-21 21:33:16 +0000

    deploy: FidelisAnalog/AES6-Wow-and-Flutter@8f021da00f69d75fbcda2c2333a4244304f3270f

--- wow/preview/mini-coi.js
+++ wow/preview/mini-coi.js
@@ -19,7 +19,7 @@
         if (!status || status > 399) return r;
         const h = new Headers(r.headers);
         h.set('Cross-Origin-Opener-Policy', 'same-origin');
-        h.set('Cross-Origin-Embedder-Policy', 'require-corp');
+        h.set('Cross-Origin-Embedder-Policy', 'credentialless');
         h.set('Cross-Origin-Resource-Policy', 'cross-origin');
         return new Response(body, { status, statusText, headers: h });
       }));
